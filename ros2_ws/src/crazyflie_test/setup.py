01ef59c11744252b47b4ed197c0b7fc87bf2193d
Sam Xiao <samcyxiao@outlook.com>
2025-11-02 17:54:24 -0500

    scaffolding

---
+++ ros2_ws/src/crazyflie_test/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+package_name = 'crazyflie_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='uav',
+    maintainer_email='samcyxiao@outlook.com',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'hello_world = crazyflie_test.hello_world:main'
+        ],
+    },
+)
